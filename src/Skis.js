86e2e0617066ffa8c7a00cb8fe43f42daa4ebac9
dziobakwszafie <dziobakwszafie@gmail.com>
2019-12-22 15:09:38 +0100

    changing branch

--- src/Skis.js
+++ src/Skis.js
@@ -16,22 +16,46 @@ import {
 
 class Skis extends Component {
     render() {
-        const ski = ['Voelkl', '175', '16.9' ];
-        const counting = ski.map( ski => {
             return (
-                <p className="btn btn-outline-info">{ski}</p>  
+                
             )
     });
 
         return (
-            <div>{counting}</div>
+           
         );
     }
 }
+
+const allThings = [
+    {
+        nazwa: 'Atom',
+        długośc: '180cm'
+    },
+    {
+        nazwa: 'Voelkl',
+        długośc: '164cm'
+    }
+]
+
 export default Skis;
 
 
 
-//moze by tak tytuł param\etru opisac na sztywno, 
-//potem zrobic tablice w tablicy i z pierwszej tablicy przpisac obiekty do danego parametru 
-// a w drugien zrobić funkcję map zeby się tworzyło wiecej elementów 
+
+
+// class Skis extends Component {
+//     render() {
+//         const ski = ['Voelkl', '175', '16.9' ];
+//         const counting = ski.map( ski => {
+//             return (
+//                 <p className="btn btn-outline-info">{ski}</p>  
+//             )
+//     });
+
+//         return (
+//             <div>{counting}</div>
+//         );
+//     }
+// }
+// export default Skis;
