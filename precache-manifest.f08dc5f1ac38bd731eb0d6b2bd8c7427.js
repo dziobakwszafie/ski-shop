2ba41082541d964a200c50889b18f90027ad5991
dziobakwszafie <dziobakwszafie@gmail.com>
2020-01-17 16:52:08 +0100

    Updates

---
+++ precache-manifest.f08dc5f1ac38bd731eb0d6b2bd8c7427.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9871a2f730bfb090b8fa91a9201babc6",
+    "url": "/ski-shop/index.html"
+  },
+  {
+    "revision": "f3da69a6d2e5116c4745",
+    "url": "/ski-shop/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1394da23fbdb3bae984e",
+    "url": "/ski-shop/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "f3da69a6d2e5116c4745",
+    "url": "/ski-shop/static/js/2.9aad64ba.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ski-shop/static/js/2.9aad64ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1394da23fbdb3bae984e",
+    "url": "/ski-shop/static/js/main.d8fa466d.chunk.js"
+  },
+  {
+    "revision": "dce52d2bf6f3366a0650",
+    "url": "/ski-shop/static/js/runtime-main.93e3ffde.js"
+  },
+  {
+    "revision": "7d84cbe1556c1f4798e3ff1ed3806cb1",
+    "url": "/ski-shop/static/media/bg.7d84cbe1.jpg"
+  },
+  {
+    "revision": "4b3c0e7de192d2fbe1068f5eba750dce",
+    "url": "/ski-shop/static/media/skis1.4b3c0e7d.jpg"
+  },
+  {
+    "revision": "a0ebcc96bbf3257b6458c005bb1141bb",
+    "url": "/ski-shop/static/media/whatsapp.a0ebcc96.png"
+  }
+]);
