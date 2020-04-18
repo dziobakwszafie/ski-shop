2a6be6b76d3882293fff57baafff6a7a96550532
dziobakwszafie <dziobakwszafie@gmail.com>
2020-04-18 18:58:03 +0200

    fix: remove some unnecessary code

--- src/components/Sort/index.js
+++ src/components/Sort/index.js
@@ -1,24 +1,10 @@
 import Sort from "./Sort";
 import { connect } from "react-redux";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    data: state
+    data: state,
   };
 };
 
-// const mapDispathToProps = () => {
-//   getSkis: getSkisAction()
-// };
-
-const mapDispathToProps = dispatch => {
-  return {
-    deleteModel: id => {
-      dispatch({ type: "DELETE_MODEL", id: id });
-    }
-  };
-};
-export default connect(
-  mapStateToProps,
-  mapDispathToProps
-)(Sort);
+export default connect(mapStateToProps)(Sort);
